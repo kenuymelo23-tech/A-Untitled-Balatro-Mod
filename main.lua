20005fb8aa4d605ff8000fa55f97468ddd724237
kenuymelo23-tech <kenuymelo23@gmail.com>
2026-09-17 20:20:36 -0300

    Code Changes
    simplified this because of crashes :sob:

--- main.lua
+++ main.lua
@@ -1,4 +1,47 @@
-local jokers_src = SMODS.NFS.getDirectoryItems(SMODS.current_mod.path .. "src/jokers")
-for _, file in ipairs(jokers_src) do
-    assert(SMODS.load_file("src/jokers/" .. file))()
-end
+--#region Atlases
+
+SMODS.Atlas {
+    key = 'restlessgambler',
+    path = 'restless_gambler.png',
+    px = 71,
+    py = 95
+}
+
+
+--jokers
+
+SMODS.Joker {
+    key = 'restlessjoker',
+    -- descriptive stuff
+    loc_txt = {
+        name = 'The Restless Gambler',
+        text = {
+            "one in three chance to win big",
+            "and gain 10 mult"
+        }
+    },
+    config = {
+        extra = {
+            mult = 10
+        }
+    },
+    loc_vars = function(self, info_queue, card)
+        return {
+            vars = {
+                card.ability.extra.mult
+            }
+        }
+    end,
+    -- stuff
+    rarity = 2,
+    atlas = 'restlessgambler',
+    -- calculation
+    calculate = function(self, card, context)
+        -- context
+        if context.joker_main then
+            return {
+                mult = card.ability.extra.mult
+            }
+        end
+    end
+}
