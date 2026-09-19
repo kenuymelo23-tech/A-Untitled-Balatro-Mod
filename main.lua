dc6f4a3809ac4f9a43bb61c475a86d9689cc99b3
kenuymelo23-tech <kenuymelo23@gmail.com>
2026-09-19 17:32:16 -0300

    added evil seal n scary joker

--- main.lua
+++ main.lua
@@ -33,7 +33,7 @@ SMODS.Joker {
         }
     end,
     -- stuff
-    rarity = 2,
+    rarity = 1,
     atlas = 'jokers',
     pos = {
         x = 0,
@@ -56,4 +56,98 @@ SMODS.Joker {
             end
         end
     end
+}
+SMODS.Joker {
+    key = 'horrorjoker',
+    -- descriptive stuff
+    loc_txt = {
+        name = 'Scary Joker',
+        text = {
+            "If there is a face card",
+            "apply a Evil Seal to it."
+        }
+    },
+    config = {
+        extra = {
+            xscore = 3
+        }
+    },
+    loc_vars = function(self, info_queue, card)
+        return {
+            vars = {
+                card.ability.extra.xscore
+            }
+        }
+    end,
+    -- stuff
+    rarity = 2,
+    atlas = 'jokers',
+    pos = {
+        x = 1,
+        y = 0
+    },
+    -- calculation
+    calculate = function(self, card, context)
+        if context.before then
+            for _, scored_card in ipairs(context.scoring_hand) do
+                if scored_card:is_face() then
+                    scored_card:set_seal("utm_evilseal")
+                    return {
+                        message = {
+                            'hehehehe, thanksss',
+                            'humannn.'
+                        }
+                    }
+                end
+            end
+        end
+    end
+}
+-- seal atlas (scrapped)
+SMODS.Atlas {
+    key = "evilseal",
+    path = "evilseal.png",
+    px = 71,
+    py = 95
+}
+
+-- scrappped seal due to problems
+SMODS.Seal {
+    key = "evilseal",
+    badge_colour = HEX("1d4fd7"),
+    config = {
+        xmult = 6
+    },
+    loc_txt = {
+        -- Badge name (displayed on card description when seal is applied)
+        label = 'Evil Seal',
+        -- Tooltip description
+        name = 'Evil Seal',
+        text = {
+            'gives {X:mult,C:white}X#4#{} Mult'
+        }
+    },
+    loc_vars = function(self, info_queue, card)
+        return {
+            vars = {
+                card.ability.seal.xmult
+            }
+        }
+    end,
+    atlas = "evilseal",
+    pos = {
+        x = 0,
+        y = 0
+    },
+
+    -- self - this seal prototype
+    -- card - card this seal is applied to
+    calculate = function(self, card, context)
+        -- main_scoring context is used whenever the card is scored
+        if context.main_scoring then
+            return {
+                xmult = card.ability.seal.xmult
+            }
+        end
+    end,
 }
